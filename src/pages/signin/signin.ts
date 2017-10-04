8ceb4ba1bc6f663756caa4437f511653ac696836
pejman <naderpejman@yahoo.com>
2017-10-04 09:03:49 -0600

    video148: Creating the Signin Page (and Form).
    just the page form and makesure the onSignin function is working and outputting to the console

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {NgForm} from '@angular/forms';
 
-/**
- * Generated class for the SigninPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,7 +10,12 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SigninPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  onSignin(form:NgForm){
+    console.log(form.value);
+  }
+
+
+  constructor( ) {
   }
 
   ionViewDidLoad() {
