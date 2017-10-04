ce8cf2d89df3cdd0c7a65fbff9d54a316371aac8
pejman <naderpejman@yahoo.com>
2017-10-04 09:48:23 -0600

    Video150: AddingFirebaseToYourApplication
    Followup on video149: Authentication doesn't need user session when setup for mobile app, it only needs token.. server sends token and frontend matches that token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component,ViewChild } from '@angular/core';
 import { Platform, NavController,MenuController} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import firebase from  'firebase';
+
+
 import {TabsPage} from "../pages/tabs/tabs";
 import {SigninPage} from "../pages/signin/signin";
 import {SignupPage} from "../pages/signup/signup";
@@ -15,9 +18,15 @@ export class MyApp {
   signinPage=SigninPage;
   signupPage=SignupPage;
 
+
+
   @ViewChild('nav')nav: NavController;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menuCtrl:MenuController) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDaVXoCfCisRwFYgIihU228AC2ksxOwpNU",
+      authDomain: "ionic-recipebpook.firebaseapp.com",
+    });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
