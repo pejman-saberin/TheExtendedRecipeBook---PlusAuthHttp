90b278148efafbf3acfde5d2df048940846e388b
pejman <naderpejman@yahoo.com>
2017-10-04 08:58:14 -0600

    video147: Creating the Signup Page (and Form).Commit Before this is 146:Adding Sidemenu
    Created the form and output the object to the console

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {NgForm} from '@angular/forms';
 
-/**
- * Generated class for the SignupPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,9 +10,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  onSignup(form:NgForm){
+    console.log(form.value);
   }
 
+  constructor(){}
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
   }
