a24b74191a89295efd5353492b0517bda7b78d33
pejman <naderpejman@yahoo.com>
2017-10-04 10:35:58 -0600

    151:showingALoader(Spinner)andErrorAlert

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {NgForm} from '@angular/forms';
- 
+import {LoadingController, AlertController} from 'ionic-angular'
+
 
 import {AuthService} from "../../services/auth";
 
@@ -11,18 +12,29 @@ import {AuthService} from "../../services/auth";
   templateUrl: 'signup.html',
 })
 export class SignupPage {
-  constructor(private authService: AuthService){}
+  constructor(private authService: AuthService, private loadingCtrl:LoadingController, private alertCtrl: AlertController){}
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
   }
 
   onSignup(form:NgForm){
+    const loading = this.loadingCtrl.create({
+      content: 'Signing you up:...'
+    });
+    loading.present();
     //console.log(form.value);
     this.authService.signup(form.value.email,form.value.password)
-      .then(data=> console.log(data) )
-      .catch(error=>console.log(error));
+      .then(data=> {
+        loading.dismiss();
+      })
+      .catch(error=>{
+        loading.dismiss();
+        const alert=this.alertCtrl.create({
+          title:'Signup failed',
+          message:error.message,
+          buttons: ['ok']
+        });
+        alert.present();
+      });
   }
-
-
-
 }
