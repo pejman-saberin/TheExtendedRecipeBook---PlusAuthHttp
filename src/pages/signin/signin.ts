e3f35ee6e4eb9311b9e7eed95de2f9a9e5c2d555
pejman <naderpejman@yahoo.com>
2017-10-04 20:41:03 -0600

    video 154:Refining the Signin Page
    completing the sigin by displaying alert and spinner

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {LoadingController, AlertController} from 'ionic-angular'
+
 import {AuthService} from "../../services/auth";
 
-
-
-@IonicPage()
 @Component({
   selector: 'page-signin',
   templateUrl: 'signin.html',
 })
 export class SigninPage {
-  constructor(private authService: AuthService ) {
+  constructor(private authService: AuthService, private loadingCtrl: LoadingController, private alertCtrl: AlertController ) {
   }
 
     ionViewDidLoad() {
@@ -19,12 +18,22 @@ export class SigninPage {
 
   onSignin(form:NgForm){
     //console.log(form.value);
+    const loading=this.loadingCtrl.create({
+      content: 'Signing you in ...'
+    })
     this.authService.signin(form.value.email,form.value.password)
       .then(data=> {
-        console.log(data);
+
+        loading.dismiss();
       })
       .catch(error=>{
-        console.log(error);
+        loading.dismiss();
+        const alert=this.alertCtrl.create({
+          title: 'Signin failed',
+          message: error.message,
+          buttons: ['ok']
+        });
+        alert.present ();
       });
     }
   }
