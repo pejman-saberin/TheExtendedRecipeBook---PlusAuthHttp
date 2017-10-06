a6f1bb81d5e6e3e7393b28fe92a6c0123cabfc40
pejman <naderpejman@yahoo.com>
2017-10-06 08:13:18 -0600

    164: polishingthe App (adding a spinnner and Error Handling)
    Error handling , spinner are added.

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {NgForm} from '@angular/forms';
-import { IonicPage, NavController, NavParams,PopoverController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,PopoverController,LoadingController,AlertController } from 'ionic-angular';
 import {ShoppingListService} from "../../services/shopping-list";
 import {Ingredient} from "../../models/ingredient";
 //import {SLOptionsPage} from "../../pages/shopping-list/sl-options/sl-options";
@@ -22,7 +22,7 @@ export class ShoppingListPage {
   ListItems: Ingredient[];
 
   constructor (private slService: ShoppingListService, private popoverCtrl: PopoverController,
-    private authService:AuthService){}
+    private authService:AuthService, private loadingController: LoadingController,private alertctrl: AlertController){}
     /*
     http://blog.ionic.io/navigating-lifecycle-events/
     ionViewWillEnter: It’s fired when entering a page, before it becomes the active one. Use it for tasks you want to do every time you enter in the view (setting event listeners, updating a table, etc.).
@@ -51,15 +51,20 @@ export class ShoppingListPage {
     }
 
     onShowOptions (event: MouseEvent){
+      const loading=this.loadingController.create({
+        content: 'Please wait...'
+      });
       const popover=this.popoverCtrl.create(SLOptionsPage); //popover is like a modal
       popover.present({ev:event});
       popover.onDidDismiss(data=>{
         if (data.action=='load'){
+          loading.present();
           this.authService.getActiveUser().getToken()
           .then((token: string)=>{
             this.slService.fetchList(token)
             .subscribe(
               (list: Ingredient[])=>{
+                loading.dismiss();
                 if(list){
                   console.log ('Fetch Success!');
                   this.ListItems=list;
@@ -69,18 +74,27 @@ export class ShoppingListPage {
                 }
               },
               error=>{
+                loading.dismiss();
+                this.handleError(error.message);
                 console.log(error);
               }
             );
           });
-        }else{
+        }else if (data.action== 'store'){
+          loading.present();
           this.authService.getActiveUser().getToken()
           .then((token: string)=>{
             this.slService.storeList(token)
             .subscribe(
-              ()=>console.log ('Store Success!'),
+
+              ()=>{
+                loading.dismiss();
+                console.log ('Store Success!');
+            },
               error=>{
-                console.log(error);
+                loading.dismiss();
+                this.handleError(error.message);
+                //console.log(error);
               }
             );
 
@@ -91,6 +105,16 @@ export class ShoppingListPage {
 
     ionViewDidLoad() {
       console.log('ionViewDidLoad ShoppingListPage');
+    }
+
+    private handleError(errorMessage: string){
+      const alert=this.alertctrl.create({
+        title: 'An error occured',
+        message:errorMessage,
+        buttons: ['ok']
+      });
+       alert.present();
+
     }
 
   }
